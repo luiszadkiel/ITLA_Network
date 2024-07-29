d5f46bee3d42d98c7b37417fef7fb2f2c7d45cf6
adrian <adria@192.168.1.202>
2024-07-28 23:11:16 -0400

    commit03

--- src/itla_network/ventana_principal.java
+++ src/itla_network/ventana_principal.java
@@ -96,7 +96,7 @@ public class ventana_principal {
 		
 		JButton btnChat = new JButton("Chat");
 
-		btnChat.setBounds(0, 164, 193, 36);
+		btnChat.setBounds(0, 164, 193, 36);//s
 		btnChat.setBounds(0, 170, 193, 30);
 
 		internalFrame1.getContentPane().add(btnChat);
