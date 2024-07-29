48d834c812e94e1d431925648819a99b9ce5086f
zadkiel <zadkiel@DESKTOP-OBMRGB5>
2024-07-28 21:54:02 -0400

    mejora de la clase conexion

--- src/basedatos/Conexion_mysql.java
+++ src/basedatos/Conexion_mysql.java
@@ -7,11 +7,10 @@ public class Conexion_mysql {
     private static final String URL = "jdbc:mysql://181.36.177.174:3306/database_itlanetwork";
     private static final String USER = "prueba_remot";
     private static final String PASSWORD = "Duranaracena01";
-
-    public static void main(String[] args) {
+    {
         Connection conexion = null;
         Statement statement = null;
-
+        
         try {
             // Propiedades para la conexión
             Properties properties = new Properties();
@@ -20,9 +19,6 @@ public class Conexion_mysql {
            
             // 1. Crear conexión
             conexion = DriverManager.getConnection(URL, properties);
-
-            // 2. Crear objeto de tipo Statement
-            statement = conexion.createStatement();
           
 
         } catch (SQLException e) {
@@ -36,9 +32,6 @@ public class Conexion_mysql {
                 System.out.println("Error al cerrar los recursos: " + e.getMessage());
             }
         }
-    }
     
-    
-    
-    
-}
+   
+}}
